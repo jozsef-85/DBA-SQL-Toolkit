f190ec445863b701bb8c70b804a73f2a6a799049
Jose Leal <josalejandro@gmail.com>
2026-04-27 13:53:34 -0400

    Troubleshotting

---
+++ Troubleshooting/sesionesbloqueadas.sql
@@ -0,0 +1,16 @@
+SELECT
+    r.session_id,
+    r.blocking_session_id,
+    DB_NAME(r.database_id) AS database_name,
+    r.status,
+    r.command,
+    r.wait_type,
+    r.wait_time / 1000 AS wait_seconds,
+    r.cpu_time,
+    r.reads,
+    r.writes,
+    r.logical_reads,
+    r.start_time
+FROM sys.dm_exec_requests r
+WHERE r.blocking_session_id <> 0
+ORDER BY r.wait_time DESC;
--- transaction-log/02_log_space_and_reuse.sql
+++ transaction-log/02_log_space_and_reuse.sql
@@ -10,3 +10,26 @@ SELECT
     log_reuse_wait_desc
 FROM sys.databases
 ORDER BY name;
+
+-- Ultimo Back ejecutado
+
+USE master;
+GO
+
+SELECT
+    d.name AS database_name,
+    d.recovery_model_desc,
+    d.log_reuse_wait_desc,
+    MAX(bs.backup_finish_date) AS last_log_backup,
+    DATEDIFF(MINUTE, MAX(bs.backup_finish_date), GETDATE()) AS minutes_since_last_log_backup
+FROM sys.databases d
+LEFT JOIN msdb.dbo.backupset bs
+    ON d.name = bs.database_name
+   AND bs.type = 'L'
+WHERE d.database_id > 4
+GROUP BY
+    d.name,
+    d.recovery_model_desc,
+    d.log_reuse_wait_desc
+ORDER BY d.name;
+GO
